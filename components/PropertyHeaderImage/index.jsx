06dd992ff98ca778d0c2faa7c10aa7b87f919233
AqibNiazi <aqibjaved5201@gmail.com>
2024-04-21 13:41:55 +0500

    half-way property details page completed

---
+++ components/PropertyHeaderImage/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+const PropertyHeaderImage = ({image}) => {
+  
+  return (
+    <section>
+    <div className="container-xl m-auto">
+      <div className="grid grid-cols-1">
+        <Image
+          src={`/properties/${image}`}
+          alt="property header Image"
+          className="object-cover h-80 w-full"
+          width={0}
+          height={0}
+          sizes='100vw'
+          priority={true}
+        />
+      </div>
+    </div>
+  </section>
+  
+  )
+}
+
+export default PropertyHeaderImage
