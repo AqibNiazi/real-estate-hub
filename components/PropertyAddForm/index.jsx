39855cf1d14c9d7fd07c09721e7209ba42f2715f
AqibNiazi <aqibjaved5201@gmail.com>
2024-05-01 23:55:53 +0500

    working on backend of form submission

--- components/PropertyAddForm/index.jsx
+++ components/PropertyAddForm/index.jsx
@@ -84,7 +84,11 @@ const PropertyAddForm = () => {
   };
   return (
     mounted && (
-      <form>
+      <form
+        action="/api/properties"
+        method="POST"
+        encType="multipart/form-data"
+      >
         <h2 className="text-3xl text-center font-semibold mb-6">
           Add Property
         </h2>
