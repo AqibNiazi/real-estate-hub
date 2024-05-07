485d1d68b9e3c917c7ffeab222d962b19e934cf9
AqibNiazi <aqibjaved5201@gmail.com>
2024-05-07 17:55:41 +0500

    working on delete property

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,6 +1,6 @@
 import connectDB from "@/config/db";
 import Property from "@/models/Property";
-
+import { getSessionUser } from "@/utils/getSessionUser";
 ///Get /api/properties/:id
 export const GET = async (request, { params }) => {
   try {
@@ -13,5 +13,35 @@ export const GET = async (request, { params }) => {
   } catch (error) {
     console.log(error);
     return new Response("somethhing went wrong", { status: 500 });
+  }
+};
+///Delete /api/properties/:id
+export const Delete = async (request, { params }) => {
+  try {
+    await connectDB();
+    const propertyId = params.id;
+    const sessionUser = await getSessionUser();
+    ///check for session
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User Id is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    const property = await Property.findById(propertyId);
+
+    if (!property) return new Response("Property Not Found", { status: 404 });
+    //verify ownership
+    if (property?.owner.toString() !== userId) {
+      return new Response("un-authorized", { status: 401 });
+    }
+    await property.deleteOne();
+
+    return new Response("Property Deleted", {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Somethhing went wrong", { status: 500 });
   }
 };
--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,8 @@ import '../assets/styles/globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import AuthProvider from "@/components/AuthProvider";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export const metadata = {
   title: "Property Pulse | Find Perfect Rental",
   description: "Find your dream Rental Property",
@@ -17,6 +19,7 @@ const MainLayout = ({ children }) => {
           <Navbar />
           <main>{children}</main>
           <Footer />
+          <ToastContainer />
         </body>
       </html>
     </AuthProvider>
