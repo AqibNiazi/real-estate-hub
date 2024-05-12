a4d7525fd2392e24b051d4a798fc38ebcdd35de2
AqibNiazi <aqibjaved5201@gmail.com>
2024-05-12 23:45:44 +0500

    bookmarks completed

--- components/BookmarkButton/index.jsx
+++ components/BookmarkButton/index.jsx
@@ -3,10 +3,33 @@ import React, { useState, useEffect } from "react";
 import { FaBookmark } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { useSession } from "next-auth/react";
+import axios from "axios";
+
 const BookmarkButton = ({ property }) => {
   const { data: session } = useSession();
   const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(false);
   const userId = session?.user?.id;
+
+  useEffect(() => {
+    if (!userId) return;
+    const checkBookmarkStatus = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.post("/api/bookmark/check", {
+          propertyId: property._id,
+        });
+        if (res?.status === 200) {
+          setIsBookmarked(res?.data?.isBookmarked);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkBookmarkStatus();
+  }, [property._id, userId]);
 
   const handleClick = async () => {
     if (!userId) {
@@ -14,31 +37,32 @@ const BookmarkButton = ({ property }) => {
       return;
     }
     try {
-      const res = await fetch("/api/bookmark", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          propertyId: property._id,
-        }),
+      const res = await axios.post("/api/bookmark", {
+        propertyId: property._id,
       });
       if (res?.status === 200) {
-        const data = await res.json();
-        toast.success(data.message);
-        setIsBookmarked(data?.isBookmarked);
+        toast.success(res?.data?.message);
+        setIsBookmarked(res?.data?.isBookmarked);
       }
     } catch (error) {
       console.log(error);
       toast.error("something went wrong");
     }
   };
+
+  if (loading) return <p className="text-center">Loading...</p>;
+
   return (
     <button
       onClick={handleClick}
-      className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+      className={`${
+        isBookmarked
+          ? "bg-red-500 hover:bg-red-600"
+          : "bg-blue-500 hover:bg-blue-600"
+      }  text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center`}
     >
-      <FaBookmark className="mr-2" /> Bookmark Property
+      <FaBookmark className="mr-2" />{" "}
+      {isBookmarked ? "Remove Bookmark" : "Bookmark Property"}
     </button>
   );
 };
